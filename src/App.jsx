3ae14bd2db2d8ac17a7151ed6b873793295bfab9
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-25 23:21:15 +0530

    Add routing for Login, SignUp, Order, and AdminDashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Resturant from "./pages/Restaurant";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Order from "./pages/Order"; 
+import AdminDashboard from "./pages/AdminDashboard";
+
 
 const App = () => {
   return (
@@ -11,6 +16,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/restaurant" element={<Resturant />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/dashboard" element={<AdminDashboard/>} />
+        
       </Routes>
     </BrowserRouter>
   );
