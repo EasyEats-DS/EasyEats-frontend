77bf8cd169b865d5cee055bc0f35a9bd1221ec97
danuka-nalidu <danukanalidu2002@gmail.com>
2025-04-26 23:03:53 +0530

    Add Notification section to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,6 +90,15 @@ const Home = () => {
       svgPath:
         "M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
       buttonText: "Reset Password",
+      title: "Notification",
+      description: "Check your notifications",
+      link: "/notification",
+      colorClass: "border-red-500",
+      buttonClass: "bg-red-500 hover:bg-red-600",
+      iconColorClass: "text-red-500",
+      svgPath:
+        "M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z",
+      buttonText: "View Notifications",
     },
   ];
 
