95acbae21ce9ea2a0972bd8461ef6ae0bf623a17
banuka1 <banukawahalathanthri@gmail.com>
2025-04-28 11:21:47 -0700

    remove > in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ const App = () => {
           <Route path="/stripepayment" element={<ProtectedRoute><StripePayment /></ProtectedRoute>} />
         </Routes>
       </Elements>
->
+
     </BrowserRouter>
   );
 };
